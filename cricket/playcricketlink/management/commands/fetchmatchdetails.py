526c929f856cc4e9f03369b9e566dfcae2d939d0
Riley Evans <riley@rileyevans.co.uk>
2019-04-15 00:06:58 +0100

    Small tweaks

--- cricket/playcricketlink/management/commands/fetchmatchdetails.py
+++ cricket/playcricketlink/management/commands/fetchmatchdetails.py
@@ -40,10 +40,12 @@ class Command(BaseCommand):
             instance = pc_instance[0].link
         else:
             instance = corecls.objects.create(**save_data)
+            instance.save()
             pc_instance = cls.objects.create(**{
                 'pc_id': pc_id,
                 'link_id': instance.id,
             })
+            pc_instance.save()
         return instance.id
 
     def process(self, match):
@@ -53,6 +55,7 @@ class Command(BaseCommand):
             match_data = self.get_data(
                 'match_detail.json?match_id={}'.format(match_id)
             )['match_details'][0]  # Gets match data from API
+            # print(match_data)
             if match_id == str(match_data['id']):  # Check correct match is returned
                 # create match model
                 if pcmodels.Match.objects.filter(pc_id=match_id).count() == 0:  # if not in database
@@ -136,7 +139,6 @@ class Command(BaseCommand):
                         match_data['away_team_id']
                     )
 
-
                     league_id = self.save_detail(
                         pcmodels.League,
                         {
@@ -144,8 +146,6 @@ class Command(BaseCommand):
                         },
                         match_data['league_id']
                     ) if match_data['league_id'] != '' else 1
-
-
 
                     # Competition
                     match.competition_id = self.save_detail(
@@ -296,7 +296,6 @@ class Command(BaseCommand):
                             match_data['ground_id']
                         ) if match_data['ground_id'] != '' else 1
 
-
                         # Match Date
                         match_date = datetime.strptime(match_data['match_date'], '%d/%m/%Y').date()
                         if coremodels.MatchDate.objects.filter(
@@ -332,34 +331,36 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """ Default command handler. Run after __init__ """
-        club_id = pcmodels.Club.objects.filter(link__home_club=True)[0].pc_id
-        teams = pcmodels.Team.objects.filter(link__club__home_club=True)
-        # teams = PlayCricketTeam.objects.filter(active=True)
-        for team in teams:  # For each team defined in settings
-            self.stdout.write('Next team {}'.format(team.link.name))
-            season_range = [datetime.now().year]
-            # Remove comment below on first run!!
-            season_range = range(
-                int(2017),
-                int(datetime.now().year) + 1,
-                1
-            )
-            for i in season_range:  # for each season till current one
-                self.stdout.write('next season {}'.format(i))
-                matches = self.get_data(
-                    'matches.json?&site_id={}&season={}&team_id={}'.format(club_id, i, team.pc_id)
-                )['matches']  # fetches matches for team and season
-                for match in matches:
-                    # for each match in data requested
-                    # Checks match status is new and published, and not currently in database
-                    if match['status'] == 'New' and match['published'] == 'Yes':
-                        # self.queue.put({
-                        #     'match_id': match['id'],
-                        #     'team': team
-                        # })  # Adds match id to queue
-                        self.process({
-                            'match_id': match['id'],
-                            'team': team,
-                        })
-                        self.stdout.write('Added {} to queue'.format(match['id']))
+        clubs = pcmodels.Club.objects.filter(link__home_club=True)
+        for club in clubs:
+            club_id = club.pc_id
+            teams = pcmodels.Team.objects.filter(link__club_id=club.link.id)
+            # teams = PlayCricketTeam.objects.filter(active=True)
+            for team in teams:  # For each team defined in settings
+                self.stdout.write('Next team {}'.format(team.link.name))
+                season_range = [datetime.now().year]
+                # Remove comment below on first run!!
+                season_range = range(
+                    int(2017),
+                    int(datetime.now().year) + 1,
+                    1
+                )
+                for i in season_range:  # for each season till current one
+                    self.stdout.write('next season {}'.format(i))
+                    matches = self.get_data(
+                        'matches.json?&site_id={}&season={}&team_id={}'.format(club_id, i, team.pc_id)
+                    )['matches']  # fetches matches for team and season
+                    for match in matches:
+                        # for each match in data requested
+                        # Checks match status is new and published, and not currently in database
+                        if match['status'] == 'New' and match['published'] == 'Yes':
+                            # self.queue.put({
+                            #     'match_id': match['id'],
+                            #     'team': team
+                            # })  # Adds match id to queue
+                            self.process({
+                                'match_id': match['id'],
+                                'team': team,
+                            })
+                            self.stdout.write('Added {} to queue'.format(match['id']))
         self.stdout.write('Matches have been saved')
